5bfbc9ca0ef507347aefa9b36ceb012b7db52d31
Hammad <hammad2091504@hotmail.com>
2022-08-01 15:30:01 +0000

    Mock dom test using fs= require(fs)

--- scripts/tests/button.test.js
+++ scripts/tests/button.test.js
@@ -5,12 +5,20 @@
 const buttonClick = require("../button");
 
 beforeEach(() => {
-    document.body.innerHTML = "<p id='par'></p>";
+    // document.body.innerHTML = "<p id='par'></p>";
+    let fs = require("fs");
+    let fileContents = fs.readFileSync("index.html", "utf-8");
+    document.open();
+    document.write(fileContents);
+    document.close();
 });
 
 describe("DOM test", () => {
     test("expects p contect to change", () => {
         buttonClick();
         expect(document.getElementById("par").innerHTML).toEqual("You Clicked");
+    });
+    test("h1 should exisit", () => {
+        expect(document.getElementsByTagName("h1").length).toBe(1);
     });
 });
